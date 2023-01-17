b01b9a2647bd38f5178524af6aadf6b587852868
Sundarapandiyan <en.pandiyan@gmail.com>
2023-01-17 10:43:46 +0530

    lectures

---
+++ oop/src/abstractDemo/Main.java
@@ -0,0 +1,12 @@
+package abstractDemo;
+
+public class Main {
+    public static void main(String[] args) {
+        Son son = new Son(30);
+        son.career();
+
+        Parent daughter = new Daughter(25);
+        daughter.career();
+    }
+}
+
---
+++ oop/src/abstractDemo/Parent.java
@@ -0,0 +1,13 @@
+package abstractDemo;
+
+public abstract class Parent {
+
+    int age;
+
+    public Parent(int age) {
+        this.age = age;
+    }
+
+    abstract void career();
+    abstract void partner();
+}
