f3d09d025cb0d116aecdaf74ce9f58f7d0427e12
Sundarapandiyan <en.pandiyan@gmail.com>
2023-01-22 20:00:56 +0530

    lectures

--- LinkedList/src/LL.java
+++ LinkedList/src/LL.java
@@ -49,6 +49,22 @@ public class LL {
         size++;
     }
 
+    public void insertRec(int val, int index) {
+        head = insertRec(val, index, head);
+    }
+
+    private Node insertRec(int val, int index, Node node) {
+        if (index == 0) {
+            Node temp = new Node(val, node);
+            size++;
+            return temp;
+        }
+
+        node.next = insertRec(val, index-1, node.next);
+        return node;
+    }
+
+
     public int deleteFirst() {
         int val = head.value;
         head = head.next;
@@ -125,5 +141,65 @@ public class LL {
             this.value = value;
             this.next = next;
         }
+    }
+
+
+
+    // leetcode problem remove duplicate
+    public void duplicates(){
+        Node node = head;
+        while (node.next != null) {
+            if(node.value == node.next.value) {
+                node.next = node.next.next;
+                size--;
+            } else {
+                node = node.next;
+            }
+        }
+        tail = node;
+        tail.next = null;
+    }
+
+    // merge
+    public static LL merge(LL first, LL second){
+        Node f = first.head;
+        Node s = second.head;
+
+        LL ans = new LL();
+        while (f != null && s != null) {
+            if (f.value < s.value) {
+                ans.insertLast(f.value);
+                f = f.next;
+            } else {
+                ans.insertLast(s.value);
+                s = s.next;
+            }
+        }
+        while (f != null) {
+            ans.insertLast(f.value);
+            f = f.next;
+        }
+        while (s != null) {
+            ans.insertLast(s.value);
+            s = s.next;
+        }
+        return  ans;
+    }
+
+    public static void main(String[] args) {
+        LL first = new LL();
+        LL second = new LL();
+
+        first.insertLast(1);
+        first.insertLast(3);
+        first.insertLast(4);
+
+        second.insertLast(1);
+        second.insertLast(2);
+        second.insertLast(6);
+        second.insertLast(9);
+
+        LL ans = LL.merge(first, second);
+        ans.display();
     }
 }
--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -1,29 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-//        LL list =  new LL();
-//        list.insertFirst(3);
-//        list.insertFirst(4);
-//        list.insertFirst(5);
-//        list.insertFirst(8);
-//        list.insertFirst(1);
-//        list.insertLast(34);
-//        list.insert(45,3);
-//        list.display();
-//        System.out.println(list.deleteFirst());
-//        list.display();
-//        System.out.println(list.deleteLast());
-//        list.display();
-//        System.out.println(list.delete(2));
-//        list.display();
+        LL list =  new LL();
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertFirst(5);
+        list.insertFirst(8);
+        list.insertFirst(1);
+        list.insertLast(34);
+        list.insert(45,2);
+        list.display();
+        System.out.println(list.deleteFirst());
+        list.display();
+        System.out.println(list.deleteLast());
+        list.display();
+        System.out.println(list.delete(2));
+        list.display();
+        list.insertRec(88, 3);
+        list.display();
 
-        CLL list = new CLL();
-        list.insert(23);
-        list.insert(40);
-        list.insert(51);
-        list.insert(85);
-        list.insert(34);
-        list.display();
-        list.delete(51);
-        list.display();
+//        CLL list = new CLL();
+//        list.insert(23);
+//        list.insert(40);
+//        list.insert(51);
+//        list.insert(85);
+//        list.insert(34);
+//        list.display();
+//        list.delete(51);
+//        list.display();
     }
 }
